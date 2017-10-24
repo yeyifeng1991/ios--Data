737ebd72040f3cdc037edc56e97976d55582b723
yeyifeng <yifeng.ye@suanzi.cn>
2017-10-24 09:26:48 +0800

    Initial Commit

---
+++ xinxuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinxuan
+//
+//  Created by 叶子 on 2017/10/24.
+//  Copyright © 2017年 叶义峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xinxuan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xinxuan
+//
+//  Created by 叶子 on 2017/10/24.
+//  Copyright © 2017年 叶义峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
